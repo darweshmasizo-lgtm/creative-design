a2160acc8fbcda1097364e42ca24b43aa29f4091
darweshmasizo-lgtm <darweshmasizo@gmail.com>
2026-03-10 23:12:21 +0300

    add .change.story

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ document.querySelectorAll('.story-btn').forEach(btn =>
 {
     btn.addEventListener('click',()=>{
         btn.classList.toggle('change')
+        btn.nextElementSibling.classList.toggle
+        ('change')
     })
 })
